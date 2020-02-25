9e6a60a450ef7689a60b81f61b1e9f4c218ab1f6
Kazi Srabon <kaziiit@gmail.com>
2020-02-25 15:39:36 -0700

    Copyright added

--- PSGC_2.0/src/PowerSystemDrawingGUI/RelationshipEdge.java
+++ PSGC_2.0/src/PowerSystemDrawingGUI/RelationshipEdge.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2011 The Florida State University Research Foundation, Inc. All right reserved.
+ *
+ * Developer:
+ * Kazi Solaiman Ahmed
+ * Juan Diego Colmenares F.
+ * Dominik Neumayr
+ * Svetlana V. Poroseva
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ */
+
 package PowerSystemDrawingGUI;
 
 import org.jgrapht.graph.DefaultEdge;
